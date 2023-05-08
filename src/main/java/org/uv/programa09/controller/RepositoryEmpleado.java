1e7ddc5106017a91e35ccd7710057ba4c6445101
Itzell <mmitzel2435@gmail.com>
2023-05-07 18:13:56 -0600

    create controller

---
+++ src/main/java/org/uv/programa09/controller/RepositoryEmpleado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.uv.programa09.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ *
+ * @author emili
+ */
+public interface RepositoryEmpleado extends CrudRepository<Empleado, Long>  {
+    
+}
--- src/test/java/org/uv/programa09/Practica09ApplicationTests.java
+++ src/test/java/org/uv/programa09/Practica09ApplicationTests.java
@@ -1,4 +1,4 @@
-package org.uv.Practica09;
+package org.uv.programa09;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
